7ce23debead86a85d05dfeacd2a54bff6f7df226
Peter Grayson <jpgrayson@gmail.com>
2016-04-22 14:51:53 -0400

    Use simpler, keyword-only partial_format() algorithm

--- desmod/util.py
+++ desmod/util.py
@@ -3,62 +3,21 @@ import string
 _formatter = string.Formatter()
 
 
-def partial_format(format_string, *args, **kwargs):
-    used_args = set()
-    result, _ = _partial_format(format_string, args, kwargs, used_args)
-    _formatter.check_unused_args(used_args, args, kwargs)
-    return result
-
-
-def _partial_format(format_string, args, kwargs, used_args, recursion_depth=2,
-                    auto_arg_index=0):
-    if recursion_depth < 0:
-        raise ValueError('Max string recursion exceeded')
+def partial_format(format_string, **kwargs):
     result = []
-    for literal_text, field_name, format_spec, conversion in \
-            _formatter.parse(format_string):
-
-        if literal_text:
-            result.append(literal_text)
-
-        if field_name is not None:
-            if not field_name:
-                if auto_arg_index is None:
-                    raise ValueError('cannot switch from manual field '
-                                     'numbering to automatic field numbering')
-                field = str(auto_arg_index)
-                auto_arg_index += 1
-            elif field_name.isdigit():
-                if auto_arg_index:
-                    raise ValueError('cannot switch from automatic field '
-                                     'numbering to manual field numbering')
-                auto_arg_index = None
-                field = field_name
+    for literal, field, spec, conversion in _formatter.parse(format_string):
+        if literal:
+            result.append(literal)
+        if field is not None:
+            spec_list = [field]
+            if conversion:
+                spec_list.extend(['!', conversion])
+            if spec:
+                spec_list.extend([':', spec])
+            inner_spec = ''.join(spec_list)
+            formatted_inner = partial_format(inner_spec, **kwargs)
+            if not field or field.isdigit() or field not in kwargs:
+                result.extend(['{{', formatted_inner, '}}'])
             else:
-                field = field_name
-
-            try:
-                obj, arg_used = _formatter.get_field(field, args, kwargs)
-            except (KeyError, IndexError):
-                spec_list = []
-                if field_name or auto_arg_index is None:
-                    spec_list.append(field)
-                if conversion:
-                    spec_list.extend(['!', conversion])
-                if format_spec:
-                    spec_list.extend([':', format_spec])
-                inner_spec = ''.join(spec_list)
-                formatted_inner_spec, auto_arg_index = _partial_format(
-                    inner_spec, args, kwargs, used_args, recursion_depth-1,
-                    auto_arg_index)
-                result.extend(['{', formatted_inner_spec, '}'])
-            else:
-                used_args.add(arg_used)
-                obj = _formatter.convert_field(obj, conversion)
-                format_spec, auto_arg_index = _partial_format(
-                    format_spec, args, kwargs, used_args, recursion_depth-1,
-                    auto_arg_index)
-
-                result.append(format(obj, format_spec))
-
-    return ''.join(result), auto_arg_index
+                result.extend(['{', formatted_inner, '}'])
+    return ''.join(result).format(**kwargs)
--- tests/test_util.py
+++ tests/test_util.py
@@ -3,16 +3,13 @@ import pytest
 from desmod.util import partial_format
 
 
-@pytest.mark.parametrize('expected, format_str, args, kwargs', [
-    ('abc', 'abc', [], {}),
-    ('aBc', 'a{b}c', [], {'b': 'B'}),
-    ('aBc', 'a{}c', ['B'], {}),
-    ('a 1.00 c', 'a {:.{digits}f} c', [1], {'digits': 2}),
-    ('a {:.2f} c', 'a {:.{digits}f} c', [], {'digits': 2}),
-    ('A{b}C', '{a}{b}{c}', [], {'a': 'A', 'c': 'C'}),
-    ('{a}BC{}', '{a}{}{c}{}', ['B'], {'c': 'C'}),
-    ('01{2.name}', '{0}{1}{2.name}', [0, 1], {}),
+@pytest.mark.parametrize('expected, format_str, kwargs', [
+    ('abc', 'abc', {}),
+    ('aBc', 'a{b}c', {'b': 'B'}),
+    ('a{b!r}c', 'a{b!r}c', {}),
+    ("a'B'c", 'a{b!r}c', {'b': 'B'}),
+    ('a {:.2f} c', 'a {:.{digits}f} c', {'digits': 2}),
+    ('A{b}C', '{a}{b}{c}', {'a': 'A', 'c': 'C'}),
 ])
-def test_partial_format(expected, format_str, args, kwargs):
-    result = partial_format(format_str, *args, **kwargs)
-    assert expected == result
+def test_partial_format(expected, format_str, kwargs):
+    assert expected == partial_format(format_str, **kwargs)
