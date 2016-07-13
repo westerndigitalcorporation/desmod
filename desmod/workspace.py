1b3bbe455904cfd19770ce4be2084edb105b54be
Peter Grayson <jpgrayson@gmail.com>
2016-07-13 14:36:28 -0400

    Handle workspace dir of '.'
    Do not attempt to overwrite the workspace directory when the workspace
    directory is the current directory. This allows for the case where the user
    wants the simulation artifacts in their current directory--i.e.  when the user
    runs the simulation from their target workspace directory.

--- desmod/workspace.py
+++ desmod/workspace.py
@@ -6,14 +6,16 @@ class Workspace(object):
     def __init__(self, workspace, overwrite=False):
         self.workspace = workspace
         self.overwrite = overwrite
-        self.prev_dir = None
+        self.prev_dir = os.getcwd()
 
     def __enter__(self):
-        if self.overwrite and os.path.isdir(self.workspace):
-            shutil.rmtree(self.workspace)
-        os.makedirs(self.workspace)
-        self.prev_dir = os.getcwd()
-        os.chdir(self.workspace)
+        if os.path.relpath(self.workspace) != os.curdir:
+            workspace_exists = os.path.isdir(self.workspace)
+            if self.overwrite and workspace_exists:
+                shutil.rmtree(self.workspace)
+            if self.overwrite or not workspace_exists:
+                os.makedirs(self.workspace)
+            os.chdir(self.workspace)
 
     def __exit__(self, *exc):
         os.chdir(self.prev_dir)
