300f3bc097cc1188f50f736bae98d25eb1ed8933
Pete Grayson <pete@jpgrayson.net>
2020-04-02 17:29:22 -0400

    PEP-517 build system updates
    Add PEP-517 build-system section to pyproject.toml along with
    tool.setuptools_scm section.
    
    Move bulk of setup.py into setup.cfg

--- setup.py
+++ setup.py
@@ -1,39 +1,4 @@
 # encoding: utf-8
 from setuptools import setup
 
-setup(
-    name='desmod',
-    author='Peter Grayson',
-    author_email='pete@jpgrayson.net',
-    description='Discrete Event Simulation Modeling using SimPy',
-    long_description='\n\n'.join(
-        open(f, 'rb').read().decode('utf-8')
-        for f in ['README.rst', 'LICENSE.txt', 'CHANGELOG.rst']
-    ),
-    url='https://desmod.readthedocs.io/',
-    download_url='https://github.com/SanDisk-Open-Source/desmod',
-    license='MIT',
-    setup_requires=['setuptools_scm'],
-    use_scm_version=True,
-    install_requires=['simpy', 'pyvcd', 'PyYAML'],
-    packages=['desmod'],
-    include_package_data=True,
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Environment :: Console',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Education',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
-        'Topic :: Scientific/Engineering',
-    ],
-)
+setup(use_scm_version=True)
